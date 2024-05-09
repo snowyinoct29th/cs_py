345daad6deaf8906e616835eb159324930ba4a44
snowyinoct29th <150814878+snowyinoct29th@users.noreply.github.com>
2024-05-09 11:47:27 +0800

    Update getsALL.py

--- getsALL.py
+++ getsALL.py
@@ -22,11 +22,8 @@ chrome_driver_path = 'C:/Program Files/Google/Chrome/Application/chromedriver.ex
 def crawl_buff(page_range, output_file):
     # 你现有的用于爬取buff.163.com的代码
     # 定义伪装的用户代理头和 Cookie
-    user_agent = ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 '
-                  'Safari/537.36 Edg/124.0.0.0')
-    cookie = ('Device-Id=0hqd5aJRnp4zI7IvErHX; qr_code_verify_ticket=db26M5Qebc4bba670e5f37bdc70bddd7f575; '
-              'session=1-P-cDLSuM0n_u97BUuO8cutW2wizEsYMHPg4PdJRFvSmQ2034027570; Locale-Supported=zh-Hans; game=csgo; '
-              'csrf_token=Ijg1NjM4ZTIzOWZiMGIzZTFiZjhhZTU2ZjZkZjIwMGUzMmRkYmY2ZDMi.GRZmLA.y9jzwa30T1Zr6nam5ojrIn-Yuu4')
+    user_agent = ()
+    cookie = ()
     # 设置 Chrome 选项
     chrome_options = Options()
     chrome_options.add_argument(f'user-agent={user_agent}')
@@ -106,18 +103,8 @@ def crawl_buff(page_range, output_file):
 def crawl_c5(page_range, output_file):
     # 你现有的用于爬取c5game.com的代码
     # 定义伪装的用户代理头和 Cookie
-    user_agent = ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 '
-                  'Safari/537.36')
-    cookie = ('aliyungf_tc=6957b67b20d8c0430a47a983b2401edf09fbe61992c11c23bb0664607991b5ed; '
-              'NC5_deviceId=171473162238036281; noticeList=%5B%22196%22%5D; hideNotice=0; '
-              'Hm_lvt_86084b1bece3626cd94deede7ecf31a8=1714731651; _bl_uid=U4laRvUgqaniFOytkfCz1jy3ysaL; '
-              'NC5_crossAccessToken=undefined; Hm_lpvt_86084b1bece3626cd94deede7ecf31a8=1714731735; '
-              'tfstk=f9vBkBM82y4IQEK1r6nNCkhKgQ6SueM4RusJm3eU29BLyzKA7pPpLXv7FNTl86WK-aUWW9CkTa7FPTtklcuq3x'
-              '-HxTfxuqk2HfntHTb-UTpvsIdFequqQx-HxTXJa8FYI2'
-              '-OSgsLJwLRBlIf4TId2Jn1Wgs0wyLRectOSgh5wrSS4wtIzl6xiefckHQ9YHJOAr7vvNw8e0SCOf'
-              '-d58eJCCbs_kbKil7lLwxhAUD0K9CWwpW9pVHRHIY6FsBjaDXvK3dDwh4IVwKw'
-              '-K6ph2wFb__B6pC_28KCIBCyNegQtwde5sjO6uMcbE79xpdsqPKpuw6dX1kxchCJ_pfDLVeCHIxFLQLx7W7pGgIPy'
-              '-7XUtV7fs25fZosf7VP1Czk7Q04Cz1djM6sfcaCB6IGfZosf7Vl9GjCCci_ROC..')
+    user_agent = ()
+    cookie = ()
 
     # 设置 Chrome 选项
     chrome_options = Options()
@@ -181,21 +168,8 @@ def crawl_c5(page_range, output_file):
 def crawl_igxe(page_range, output_file):
     # 你现有的用于爬取igxe.cn的代码
     # 定义伪装的用户代理头和 Cookie
-    user_agent = ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 '
-                  'Safari/537.36 Edg/124.0.0.0')
-    cookie = ('aliyungf_tc=c7fbf4cb3220824d2eb37d9335affa554918de8d8f1d6b5fed6a0dd8855cccba; '
-              'ARK_STARTUP=eyJTVEFSVFVQIjp0cnVlLCJTVEFSVFVQVElNRSI6IjIwMjQtMDUtMDUgMTk6MTU6NDcuMDM0In0%3D; '
-              'ARK_ID=JS922db7ba4561f7c0c354bb8ab885680d922d; qimo_seosource_0=%E7%AB%99%E5%86%85; qimo_seokeywords_0=; '
-              'uuid_572d9ba0-d737-11e8-970c-a553533099d1=e949f0ec-befd-482b-845e-2798916a4de4; '
-              'qimo_seosource_572d9ba0-d737-11e8-970c-a553533099d1=%E7%AB%99%E5%86%85; '
-              'qimo_seokeywords_572d9ba0-d737-11e8-970c-a553533099d1=; '
-              'qimo_xstKeywords_572d9ba0-d737-11e8-970c-a553533099d1=; '
-              'href=https%3A%2F%2Fwww.igxe.cn%2Fmarket%2Fcsgo%3Fsort%3D3%26keyword%3DP250%25EF%25BC%2588%25E7%25BA%25AA'
-              '%25E5%25BF%25B5%25E5%2593%2581%25EF%25BC%2589%2520%257C%2520%25E6%25B2%2599%25E4%25B8%2598%25E4%25B9%258B'
-              '%25E9%25BB%2584; accessId=572d9ba0-d737-11e8-970c-a553533099d1; pageViewNum=1; '
-              'FZ_STROAGE.igxe.cn'
-              '=eyJTRUVTSU9OSUQiOiJkYTk2MTNkNDcwOGNkOTdjIiwiU0VFU0lPTkRBVEUiOjE3MTQ5MDc3NjE1MDEsIkFOU0FQUElEIjoiZGNiNjIzNWM2NjNlMGRlYSIsIkFOUyRERUJVRyI6MCwiQU5TVVBMT0FEVVJMIjoiaHR0cHM6Ly9hbmFseXNlLmlneGUuY24vIiwiRlJJU1REQVkiOiIyMDI0MDUwNSIsIkZSSVNUSU1FIjpmYWxzZSwiQVJLX0lEIjoiSlM5MjJkYjdiYTQ1NjFmN2MwYzM1NGJiOGFiODg1NjgwZDkyMmQiLCJBUktGUklTVFBST0ZJTEUiOiIyMDI0LTA1LTA1IDE5OjE1OjQ3LjAzNyJ9')
-
+    user_agent = ()
+    cookie = ()
     # 设置 Chrome 选项
     chrome_options = Options()
     chrome_options.add_argument(f'user-agent={user_agent}')
